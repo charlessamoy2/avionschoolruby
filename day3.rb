d58165c799e6fe788c23a5a7f1c05e0a0f3a8446
charlessamoy2 <peter.samoy@obf.ateneo.edu>
2020-10-29 19:37:04 +0800

    hello there

--- day3.rb
+++ day3.rb
@@ -1,24 +1,31 @@
 #Activity 1
+
+#Global Storage
 $store = []
 
-class User 
+#User Class
+class User
+    #initialize with email, username, and password
     def initialize(email,username,password)
         @email = email
         @username = username
         @password = password
     end
 
+    #add to global storage
     def addToUserList
         $store << [@email,@username,@password]
         puts "Added user to list"
         print $store.to_s + "\n"
     end
 
+    #welcomes the user
     def welcome
         puts "Welcome #{@username}! You have logged in."
     end
 end
 
+#Do everything
 charles = User.new("charlessamoy@gmail.com","charlessamoy","hellooo")
 charles.addToUserList
 charles.welcome

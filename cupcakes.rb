a4bb949d475ed488ef61036735b2e6e41d051109
charlessamoy2 <peter.samoy@obf.ateneo.edu>
2020-10-30 21:31:56 +0800

    Added assignment 2

---
+++ cupcakes.rb
@@ -0,0 +1,34 @@
+module Bake
+    def baking
+        puts "Baking #{@name}"
+    end
+end
+
+class Confection
+    include Bake
+
+    def initialize(name)
+        @name = name
+    end
+end
+
+class Cupcake < Confection
+    def prepare
+        puts "Baking at 350 degrees for 25 minutes"
+        puts "Applying frosting "
+    end
+end
+
+class BananaCake < Confection
+    def prepare
+        puts "Baking at 350 degrees for 25 minutes"
+    end
+end
+
+cupcake = Cupcake.new("Red Velvet Cupcake")
+cupcake.baking
+cupcake.prepare
+
+bananaCake = BananaCake.new("Banana Cake")
+bananaCake.baking
+bananaCake.prepare
